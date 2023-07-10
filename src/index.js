5ac3d9ac57fa2b0b210b18c1ff2753af6d0577b2
JoannaMikusinskaNowak <joannamikusinska@wp.pl>
2023-07-11 00:44:32 +0200

    add 10th HW

--- src/index.js
+++ src/index.js
@@ -1,30 +1,83 @@
-const fetchUsersBtn = document.querySelector('.btn');
-const userList = document.querySelector('.user-list');
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-fetchUsersBtn.addEventListener('click', () => {
-  fetchUsers()
-    .then(users => renderUserList(users))
-    .catch(error => console.log(error));
-});
+const breedSelect = document.querySelector('.breed-select');
+const catInfoDiv = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-function fetchUsers() {
-  return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+function displayError(message) {
+  error.textContent = message;
+  error.style.display = 'flex';
 }
 
-function renderUserList(users) {
-  const markup = users
-    .map(user => {
-      return `<li>
-          <p><b>Name</b>: ${user.name}</p>
-          <p><b>Email</b>: ${user.email}</p>
-          <p><b>Company</b>: ${user.company.name}</p>
-        </li>`;
+function hideError() {
+  error.style.display = 'none';
+}
+
+function createParagraph(text) {
+  const p = document.createElement('p');
+  p.textContent = text;
+  return p;
+}
+
+function displayCatInfo(cat) {
+  const catImage = document.createElement('img');
+  catImage.src = cat[0].url;
+
+  const breedName = document.createElement('p');
+  breedName.textContent = cat[0].breeds[0].name;
+
+  const description = document.createElement('p');
+  description.textContent = cat[0].breeds[0].description;
+
+  const temperament = document.createElement('p');
+  temperament.textContent = cat[0].breeds[0].temperament;
+
+  catInfoDiv.innerHTML = '';
+  catInfoDiv.appendChild(catImage);
+  catInfoDiv.appendChild(breedName);
+  catInfoDiv.appendChild(description);
+  catInfoDiv.appendChild(temperament);
+}
+
+function handleBreedSelect(event) {
+  const breedId = event.target.value;
+
+  hideError();
+  catInfoDiv.innerHTML = '';
+  loader.style.display = 'flex';
+
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      displayCatInfo(cat);
+      loader.style.display = 'none';
     })
-    .join('');
-  userList.innerHTML = markup;
+    .catch(error => {
+      displayError(error.message);
+      loader.style.display = 'none';
+    });
 }
+
+function initApp() {
+  hideError();
+  loader.style.display = 'flex';
+
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+
+      breedSelect.addEventListener('change', handleBreedSelect);
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      displayError(error.message);
+      loader.style.display = 'none';
+    });
+}
+
+initApp();
